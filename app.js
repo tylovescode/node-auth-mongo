cad71cae414e6c08e43ef35008b9f02d8475ea3c
Ty Miller <ty.loves.code@gmail.com>
2018-09-13 07:14:39 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Map Global Promise - gets rid of Mongoose warning
+mongoose.Promise = global.Promise;
+
+//Connect to Mongoose
+mongoose
+  .connect(
+    'mongodb://localhost/vidjot-dev',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Method Override Middleware
+app.use(methodOverride('_method'));
+
+//Express Session Middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(flash());
+
+//Global Variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+//Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', { title: title });
+});
+
+//About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+//Use Routes
+app.use('/ideas', ideas);
+app.use('/users', users);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
