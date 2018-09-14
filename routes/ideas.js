77db055d99ded9352e1cdad8d14c97f74218421b
Ty Miller <ty.loves.code@gmail.com>
2018-09-14 18:29:52 -0400

    User can only see their entries

--- routes/ideas.js
+++ routes/ideas.js
@@ -9,7 +9,7 @@ const Idea = mongoose.model('ideas');
 
 //Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-  Idea.find({})
+  Idea.find({ user: req.user.id })
     .sort({ date: 'desc' })
     .then(ideas => {
       res.render('ideas/index', {
@@ -28,9 +28,14 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
-    res.render('ideas/edit', {
-      idea: idea
-    });
+    if (idea.user != req.user.id) {
+      req.flash('error_msg', 'Not Authorized');
+      res.redirect('/ideas');
+    } else {
+      res.render('ideas/edit', {
+        idea: idea
+      });
+    }
   });
 });
 
@@ -55,7 +60,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
   } else {
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     };
     //Saves idea to database
     new Idea(newUser).save().then(idea => {
